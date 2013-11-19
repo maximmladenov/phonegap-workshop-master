5451ce1f3a219a01a44a8b53411c991d0b2a6b4d
maxim <maximmladenov@gmail.com>
2013-11-19 16:41:12 +0200

    Cordova Feature Barcode Scanner

--- js/ScanView.js
+++ js/ScanView.js
@@ -10,17 +10,20 @@ var ScanView = function() {
         return this;
     };
 
-    this.scanQRCode = cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      }, 
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
+	var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+    this.scanQRCode = function() {
+		scanner.scan(
+		  function (result) {
+			  alert("We got a barcode\n" +
+					"Result: " + result.text + "\n" +
+					"Format: " + result.format + "\n" +
+					"Cancelled: " + result.cancelled);
+		  }, 
+		  function (error) {
+			  alert("Scanning failed: " + error);
+		  }
+	   );
+   }
 	
     this.initialize();
 
